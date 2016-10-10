834d3c1ef1b535ee37b704a7d8168ab549a11e93
ldierickx4 <laurens.drx@outlook.com>
2016-10-10 09:02:37 +0200

    first line of code

---
+++ src/geometrywars/GeometryWars.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometrywars;
+
+/**
+ *
+ * @author Laurens
+ */
+public class GeometryWars {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("this is the first line of code for meometry wars");
+    }
+    
+}
