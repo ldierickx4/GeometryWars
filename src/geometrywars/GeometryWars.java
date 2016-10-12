7a6c8d47935b7fc58fd5ee41c47aacd7eb7f00da
jorrevynckier <jorre.vynckier@student.howest.be>
2016-10-12 14:08:47 +0200

    ghj

--- src/geometrywars/GeometryWars.java
+++ src/geometrywars/GeometryWars.java
@@ -19,7 +19,7 @@ public class GeometryWars {
     public static void main(String[] args) {
         // TODO code application logic here
         GameBoard test = new GameBoard();
-        System.out.println("laurens");
+        System.out.println("Laurens");
         System.out.println("Jens");
         System.out.println("Jorre");
         System.out.println("Jordy");
