c590730fa7d61c57c3ceeff80d2d845970e4b386
jorrevynckier <jorre.vynckier@student.howest.be>
2016-10-12 14:13:55 +0200

    jah

--- src/geometrywars/GeometryWars.java
+++ src/geometrywars/GeometryWars.java
@@ -23,7 +23,7 @@ public class GeometryWars {
         System.out.println("Jens");
         System.out.println("Jorre");
         System.out.println("Jordy");
-        String zin = "Jorre fag";
+       
         
     }
     
