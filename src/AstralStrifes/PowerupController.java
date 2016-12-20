4c76dc573791cb98c8790237598f924ab372ac4a
JordyVandewalle <jordy.vandewalle@hotmail.com>
2016-12-20 10:39:09 +0100

    Powerups

--- src/AstralStrifes/PowerupController.java
+++ src/AstralStrifes/PowerupController.java
@@ -27,6 +27,7 @@ public class PowerupController implements Runnable{
     
     public void updatePowerups(){
         score = player.getScore();
+        if(score%1650 == 0 && score!=0 && powerups.size() == 0){
             Powerup powerup = new SwiftyPowerup("Swifty");
             powerups.add(powerup);
         }
